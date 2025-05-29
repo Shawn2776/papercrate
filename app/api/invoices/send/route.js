7de5f2a3c98984f1e0106850dc545dca1353089e
Shawn Harrington <shawn.harrington2776@gmail.com>
2025-05-28 19:51:37 -0700

    useCurrentUser import error resolved in resend route

--- app/api/invoices/send/route.js
+++ app/api/invoices/send/route.js
@@ -4,7 +4,7 @@ import { resend } from "@/lib/resend/resend";
 import InvoiceEmail from "@/emails/InvoiceEmail";
 import { render } from "@react-email/render";
 import { NextResponse } from "next/server";
-import { currentUser } from "@clerk/nextjs/dist/types/server";
+import { currentUser } from "@clerk/nextjs/server";
 
 export async function POST(req) {
   const user = await currentUser();
